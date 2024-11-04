cae3c0ee791a4d6c58a8d50a5330186ad3cde754
andreamirarchi <andrea.mirarchi03@gmail.com>
2024-11-04 02:20:25 +0100

    Delete Assignment-28_08_2024/src directory

--- Assignment-28_08_2024/src/js/prezzo.js
+++
@@ -1,7 +0,0 @@
-const p = 14.5;
-
-function calcolaPrezzo() {
-    const quantita = Number(document.getElementById("amount").value);
-    const prezzoTotale = p * quantita;
-    document.getElementById("prezzoCalcolato").innerText = prezzoTotale.toFixed(2);
-}
